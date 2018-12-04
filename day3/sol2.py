1e194284e43662626ce414df9f7ecaf2fa9c7813
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2018-12-04 22:39:27 +0000

    more work towards day 3

--- day3/sol2.py
+++ day3/sol2.py
@@ -3,9 +3,12 @@ from itertools import chain
 fabric = [[0 for i in range(1000)] for j in range(1000)]
 claim_re = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
 
+non_overlapping = []
+
 with open("input.txt") as f:
     for claim in f:
         matches = claim_re.search(claim)
+        claim_id = int(matches.group(1))
         begin_row = int(matches.group(2))
         begin_col = int(matches.group(3))
         width = int(matches.group(4))
@@ -13,7 +16,17 @@ with open("input.txt") as f:
         for row in range(begin_row, begin_row + width):
             for column in range(begin_col, begin_col + height):
                 cell = fabric[row][column]
-                if cell == 0 or cell == 1:   # If fabric not used yet: 0 -> 1
-                    fabric[row][column] += 1 # if already used: 1 -> 2
-print([i for i in chain.from_iterable(fabric)].count(2))
-    # chain.from_iterable basically flattens the array of arrays and turns it into a long line
+                no_collision = True
+                if cell == 0:
+                    fabric[row][column] = claim_id
+                else:
+                    if cell in non_overlapping:
+                        non_overlapping.remove(cell)
+                    no_collision = False
+                    break
+            if not no_collision:
+                break
+        if no_collision:
+            non_overlapping.append(claim_id)
+
+print(non_overlapping)
